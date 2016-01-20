4024514e49bc1ba9a4754ade0eb1db0b7f8eeef7
David Susco <dsusco@gmail.com>
2016-01-20 08:16:53 -0500

    named the mapping function in setLists

--- src/app/app.js
+++ src/app/app.js
@@ -221,20 +221,22 @@ angular
     var army = this;
 
     army.setLists = function () {
+      function return0() { return 0; }
+
       if (army.gameSize !== undefined && army.faction !== undefined && army.faction === models.faction) {
         army.lists = {
           'Core List': {
-            Leader: models.Leader.map(function () { return 0; }),
-            Infantry: models.Infantry.map(function () { return 0; }),
-            Specialist: models.Specialist.map(function () { return 0; })
+            Leader: models.Leader.map(return0),
+            Infantry: models.Infantry.map(return0),
+            Specialist: models.Specialist.map(return0)
           }
         };
 
         if (army.gameSize.Leader > 2) {
           [1, 2].forEach(function (i) {
             army.lists['Options List #' + i] = {
-              Infantry: models.Infantry.map(function () { return 0; }),
-              Specialist: models.Specialist.map(function () { return 0; })
+              Infantry: models.Infantry.map(return0),
+              Specialist: models.Specialist.map(return0)
             };
           });
         }
