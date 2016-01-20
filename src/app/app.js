171d236d96b2284922528f1cc0fa6678539665f7
David Susco <dsusco@gmail.com>
2016-01-20 11:13:37 -0500

    character check for game size change

--- src/app/app.js
+++ src/app/app.js
@@ -89,7 +89,17 @@ angular
       }
     });
 
-    $scope.$watch('army.gameSize', army.setLists);
+    $scope.$watch('army.gameSize', function () {
+      army.setLists();
+
+      try {
+        models.Leader.concat(models.Specialist).forEach(function (model) {
+          if (model.character) {
+            $scope.$broadcast('checkCharacterSelected', model, 0);
+          }
+        });
+      } catch (ignore) {}
+    });
 
     $scope.$watch('army.faction', function (faction) {
       models.load(faction, army.setLists);
