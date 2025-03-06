b5b28a1dbb4043aa801e5ea229f18893072bdc4c
David Susco <dsusco@clarku.edu>
2025-03-06 13:44:36 -0500

    added shadcn-svelte radio group

---
+++ src/lib/components/ui/radio-group/radio-group.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { RadioGroup as RadioGroupPrimitive } from "bits-ui";
+	import { cn } from "$lib/utils.js";
+
+	let { ref = $bindable(null), class: className, value = $bindable(""), ...restProps } = $props();
+</script>
+
+<RadioGroupPrimitive.Root bind:ref bind:value class={cn("grid gap-2", className)} {...restProps} />
